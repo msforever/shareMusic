0e5cfb335ecfa7e8cf7ed7bb40cb85dfb2aad563
Mayuresh <heartbroker001@gmail.com>
2017-09-20 00:10:47 +0530

    Commit 2 List all mp3 Files

--- src/java/com/Search.java
+++ src/java/com/Search.java
@@ -6,25 +6,13 @@
 package com;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.TargetDataLine;
-import org.apache.tika.Tika;
 import java.io.File;
-import java.net.URI;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.event.EventHandler;
@@ -34,6 +22,7 @@ import javafx.scene.text.Font;
 import javafx.stage.StageBuilder;
 import javafx.stage.WindowEvent;
 import javax.swing.SwingUtilities;
+import org.apache.commons.io.FilenameUtils;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -60,7 +49,7 @@ public class Search {
            try {
                session=getSession();     
                file=new File(path);
-               System.out.println(song.getSname()+" is found at: "+file.getAbsolutePath());
+               //System.out.println(song.getSname()+" is found at: "+file.getAbsolutePath());
               session.save(temp);
            } 
            finally{
@@ -71,7 +60,7 @@ public class Search {
        
     }
     void playmedia(){
-       Media hit =new Media(file.toURI().toString());
+       Media hit = new Media(file.toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(hit);
                 mediaPlayer.play();
     }
@@ -84,9 +73,32 @@ public class Search {
         return s;
         
     }
+    public void listFiles(String dir){
+        File folder = new File(dir);
+        File[] listOfFiles = folder.listFiles();
+        int count=0;
+        for (int i = 0; i < listOfFiles.length; i++) {
+            //System.out.println("File " + listOfFiles[i].getName());
+            
+            String ext1 = FilenameUtils.getExtension(listOfFiles[i].getAbsolutePath());
+            String ext2 = listOfFiles[i].getAbsolutePath();
+            //System.out.println("Extension "+ext1);
+         if (listOfFiles[i].isFile() && "mp3".equalsIgnoreCase(ext1)) {
+             count++;
+             Song song = new Song();
+             song.setSpath(ext2);
+             addSong(song);
+        System.out.println(count+" File " + listOfFiles[i].getName()+"with extension "+ext1+"\n "+ext2);
+      } else if (listOfFiles[i].isDirectory()) {
+        System.out.println("Directory " + listOfFiles[i].getName());
+      }
+      
+    }
+    }
+          
     public static void main(String[] args) {
         
-        SwingUtilities.invokeLater(new Runnable() {
+       SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 new JFXPanel(); // this will prepare JavaFX toolkit and environment
@@ -119,7 +131,7 @@ public class Search {
         
         
         Song song= new Song();
-        song.setSid(5);
+        
         song.setSname("Mann Bharaya");
         song.setSartist("JayZ");
         String path="E:\\gal.mp3";
@@ -127,6 +139,7 @@ public class Search {
         Search s=new Search();
         s.addSong(song);
         s.playmedia();
+       // s.listFiles(path);
         
     }
   
